dbcc09e4ff0ba1c765da06a9326881c53599d3e6
dbartoszp <dbartosz.pomierny@gmail.com>
2023-05-14 16:10:58 +0200

    navbar started

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import NavbarCSS from './Navbar.module.css'
 
 export default function Navbar() {
+
+
   return (
-    <p>navbar!</p>
+    <header>
+      <nav className={NavbarCSS.navigationContainer}>
+        <ul className={NavbarCSS.navigationList}>
+          <li><a href="#">(Logo)</a></li>
+          <li><a href="#">Oferta</a></li>
+          <li><a href="#">O nas</a></li>
+          <li><a href="#">Dlaczego my?</a></li>
+          <li><a href="#">Kontakt</a></li>
+          <li><button className={NavbarCSS.btnLogin}><a className={NavbarCSS.btnLink} href="#">Zaloguj się</a></button></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
