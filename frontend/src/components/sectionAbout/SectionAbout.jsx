d0807e0c94adb9955b84afc16473b2ab6adda6a8
dbartoszp <dbartosz.pomierny@gmail.com>
2023-06-10 15:47:55 +0200

    composition fixes

--- frontend/src/components/sectionAbout/SectionAbout.jsx
+++ frontend/src/components/sectionAbout/SectionAbout.jsx
@@ -1,7 +1,4 @@
-import React from 'react';
 import SectionAboutCSS from './SectionAbout.module.css';
-import AboutHeader from './AboutHeader';
-import ColumnContainerAbout from './ColumnContainerAbout';
 
 function SectionAbout({ children }) {
 	return <div className={SectionAboutCSS.sectionAbout}>{children}</div>;
