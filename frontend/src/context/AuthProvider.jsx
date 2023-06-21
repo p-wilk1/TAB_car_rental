59583addb2786f3fa0c2887c8756a824e28cf181
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-06-21 16:55:01 +0200

    added session maintenance

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -22,13 +22,28 @@
 // // export default AuthContext;
 // export { AuthProvider, useAuth };
 
-import { createContext, useState } from "react";
+import {createContext, useEffect, useState} from "react";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({});
-  console.log(auth);
+  const [auth, setAuth] = useState(() => {
+    const token = sessionStorage.getItem("token");
+    return { accessToken: token || null };
+  });
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    setAuth((prevAuth) => {
+      if (token !== prevAuth.accessToken) {
+        return { accessToken: token || null };
+      }
+      return prevAuth;
+    });
+  }, [auth]);
+
+  console.log(auth)
+
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
